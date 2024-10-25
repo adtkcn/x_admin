bdac94a6b352516bfbf8ca0d61c6e4f2d0643e95
xiangheng <11675084@qq.com>
2024-10-25 10:55:53 +0800

    后续使用no-undef-components

--- admin/.eslintrc.cjs
+++ admin/.eslintrc.cjs
@@ -27,6 +27,12 @@ module.exports = {
                 trailingComma: 'none'
             }
         ],
+        // 'vue/no-undef-components': [
+        //     'error',
+        //     {
+        //         ignorePatterns: ['el(\\-\\w+)+']
+        //     }
+        // ],
         'vue/multi-word-component-names': 'off',
         '@typescript-eslint/no-explicit-any': 'off',
         '@typescript-eslint/ban-ts-comment': 'off',
