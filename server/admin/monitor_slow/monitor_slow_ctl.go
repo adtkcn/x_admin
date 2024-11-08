fe33721abfee6745de5c215a8784aad2921328f2
xiangheng <11675084@qq.com>
2024-11-08 17:43:07 +0800

    注释

--- server/admin/monitor_slow/monitor_slow_ctl.go
+++ server/admin/monitor_slow/monitor_slow_ctl.go
@@ -21,22 +21,22 @@ type MonitorSlowHandler struct {
 	requestGroup singleflight.Group
 }
 
-//	 @Summary	监控-错误列列表
-//	 @Tags		monitor_slow-监控-错误列
-//	 @Produce	json
-//	 @Param		Token		header		string				true	"token"
-//	 @Param		PageNo		query		int					true	"页码"
-//	 @Param		PageSize	query		int					true	"每页数量"
-//		@Param ProjectKey query string false "项目key"
-//		@Param ClientId query string false "sdk生成的客户端id"
-//		@Param UserId query string false "用户id"
-//		@Param Path query string false "URL地址"
-//		@Param Time query number false "时间"
-//		@Param CreateTimeStart  query string false "创建时间"
-//		@Param CreateTimeEnd  query string false "创建时间"
+//	@Summary	监控-错误列列表
+//	@Tags		monitor_slow-监控-错误列
+//	@Produce	json
+//	@Param		Token			header		string																	true	"token"
+//	@Param		PageNo			query		int																		true	"页码"
+//	@Param		PageSize		query		int																		true	"每页数量"
+//	@Param		ProjectKey		query		string																	false	"项目key"
+//	@Param		ClientId		query		string																	false	"sdk生成的客户端id"
+//	@Param		UserId			query		string																	false	"用户id"
+//	@Param		Path			query		string																	false	"URL地址"
+//	@Param		Time			query		number																	false	"时间"
+//	@Param		CreateTimeStart	query		string																	false	"创建时间"
+//	@Param		CreateTimeEnd	query		string																	false	"创建时间"
 //
-// @Success 200	{object} response.Response{ data=response.PageResp{ lists=[]MonitorSlowResp}}	"成功"
-// @Router	/api/admin/monitor_slow/list [get]
+//	@Success	200				{object}	response.Response{ data=response.PageResp{ lists=[]MonitorSlowResp}}	"成功"
+//	@Router		/api/admin/monitor_slow/list [get]
 func (hd *MonitorSlowHandler) List(c *gin.Context) {
 	var page request.PageReq
 	var listReq MonitorSlowListReq
@@ -50,18 +50,18 @@ func (hd *MonitorSlowHandler) List(c *gin.Context) {
 	response.CheckAndRespWithData(c, res, err)
 }
 
-//		@Summary	监控-错误列列表-所有
-//		@Tags		monitor_slow-监控-错误列
-//	 @Produce	json
-//		@Param ProjectKey query string false "项目key"
-//		@Param ClientId query string false "sdk生成的客户端id"
-//		@Param UserId query string false "用户id"
-//		@Param Path query string false "URL地址"
-//		@Param Time query number false "时间"
-//		@Param CreateTimeStart  query string false "创建时间"
-//		@Param CreateTimeEnd  query string false "创建时间"
-//		@Success	200			{object}	response.Response{ data=[]MonitorSlowResp}	"成功"
-//		@Router		/api/admin/monitor_slow/listAll [get]
+//	@Summary	监控-错误列列表-所有
+//	@Tags		monitor_slow-监控-错误列
+//	@Produce	json
+//	@Param		ProjectKey		query		string										false	"项目key"
+//	@Param		ClientId		query		string										false	"sdk生成的客户端id"
+//	@Param		UserId			query		string										false	"用户id"
+//	@Param		Path			query		string										false	"URL地址"
+//	@Param		Time			query		number										false	"时间"
+//	@Param		CreateTimeStart	query		string										false	"创建时间"
+//	@Param		CreateTimeEnd	query		string										false	"创建时间"
+//	@Success	200				{object}	response.Response{ data=[]MonitorSlowResp}	"成功"
+//	@Router		/api/admin/monitor_slow/listAll [get]
 func (hd *MonitorSlowHandler) ListAll(c *gin.Context) {
 	var listReq MonitorSlowListReq
 	if response.IsFailWithResp(c, util.VerifyUtil.VerifyQuery(c, &listReq)) {
@@ -71,13 +71,13 @@ func (hd *MonitorSlowHandler) ListAll(c *gin.Context) {
 	response.CheckAndRespWithData(c, res, err)
 }
 
-// @Summary	监控-错误列详情
-// @Tags		monitor_slow-监控-错误列
-// @Produce	json
-// @Param		Token		header		string				true	"token"
-// @Param		Id		query		number				false	"错误id"
-// @Success	200			{object}	response.Response{ data=MonitorSlowResp}	"成功"
-// @Router		/api/admin/monitor_slow/detail [get]
+//	@Summary	监控-错误列详情
+//	@Tags		monitor_slow-监控-错误列
+//	@Produce	json
+//	@Param		Token	header		string										true	"token"
+//	@Param		Id		query		number										false	"错误id"
+//	@Success	200		{object}	response.Response{ data=MonitorSlowResp}	"成功"
+//	@Router		/api/admin/monitor_slow/detail [get]
 func (hd *MonitorSlowHandler) Detail(c *gin.Context) {
 	var detailReq MonitorSlowDetailReq
 	if response.IsFailWithResp(c, util.VerifyUtil.VerifyQuery(c, &detailReq)) {
@@ -91,17 +91,11 @@ func (hd *MonitorSlowHandler) Detail(c *gin.Context) {
 	response.CheckAndRespWithData(c, res, err)
 }
 
-// @Summary	监控-错误列新增
-// @Tags		monitor_slow-监控-错误列
-// @Produce	json
-// @Param		Token		header		string				true	"token"
-// @Param		ProjectKey		body		string				false	"项目key"
-// @Param		ClientId		body		string				false	"sdk生成的客户端id"
-// @Param		UserId		body		string				false	"用户id"
-// @Param		Path		body		string				false	"URL地址"
-// @Param		Time		body		number				false	"时间"
-// @Success	200			{object}	response.Response	"成功"
-// @Router		/api/admin/monitor_slow/add [post]
+//	@Summary	监控-错误列新增
+//	@Tags		monitor_slow-监控-错误列
+//	@Produce	json
+//	@Success	200	{object}	response.Response	"成功"
+//	@Router		/api/admin/monitor_slow/add [get]
 func (hd *MonitorSlowHandler) Add(c *gin.Context) {
 	data, err := url.QueryUnescape(c.Query("data"))
 	if err != nil {
@@ -116,18 +110,18 @@ func (hd *MonitorSlowHandler) Add(c *gin.Context) {
 	c.Data(200, "image/gif", img_util.EmptyGif())
 }
 
-// @Summary	监控-错误列编辑
-// @Tags		monitor_slow-监控-错误列
-// @Produce	json
-// @Param		Token		header		string				true	"token"
-// @Param		Id		body		number				false	"错误id"
-// @Param		ProjectKey		body		string				false	"项目key"
-// @Param		ClientId		body		string				false	"sdk生成的客户端id"
-// @Param		UserId		body		string				false	"用户id"
-// @Param		Path		body		string				false	"URL地址"
-// @Param		Time		body		number				false	"时间"
-// @Success	200			{object}	response.Response	"成功"
-// @Router		/api/admin/monitor_slow/edit [post]
+//	@Summary	监控-错误列编辑
+//	@Tags		monitor_slow-监控-错误列
+//	@Produce	json
+//	@Param		Token		header		string				true	"token"
+//	@Param		Id			body		number				false	"错误id"
+//	@Param		ProjectKey	body		string				false	"项目key"
+//	@Param		ClientId	body		string				false	"sdk生成的客户端id"
+//	@Param		UserId		body		string				false	"用户id"
+//	@Param		Path		body		string				false	"URL地址"
+//	@Param		Time		body		number				false	"时间"
+//	@Success	200			{object}	response.Response	"成功"
+//	@Router		/api/admin/monitor_slow/edit [post]
 func (hd *MonitorSlowHandler) Edit(c *gin.Context) {
 	var editReq MonitorSlowEditReq
 	if response.IsFailWithResp(c, util.VerifyUtil.VerifyJSON(c, &editReq)) {
@@ -136,13 +130,13 @@ func (hd *MonitorSlowHandler) Edit(c *gin.Context) {
 	response.CheckAndRespWithData(c, editReq.Id, MonitorSlowService.Edit(editReq))
 }
 
-// @Summary	监控-错误列删除
-// @Tags		monitor_slow-监控-错误列
-// @Produce	json
-// @Param		Token		header		string				true	"token"
-// @Param		Id		body		number				false	"错误id"
-// @Success	200			{object}	response.Response	"成功"
-// @Router		/api/admin/monitor_slow/del [post]
+//	@Summary	监控-错误列删除
+//	@Tags		monitor_slow-监控-错误列
+//	@Produce	json
+//	@Param		Token	header		string				true	"token"
+//	@Param		Id		body		number				false	"错误id"
+//	@Success	200		{object}	response.Response	"成功"
+//	@Router		/api/admin/monitor_slow/del [post]
 func (hd *MonitorSlowHandler) Del(c *gin.Context) {
 	var delReq MonitorSlowDelReq
 	if response.IsFailWithResp(c, util.VerifyUtil.VerifyJSON(c, &delReq)) {
@@ -154,11 +148,11 @@ func (hd *MonitorSlowHandler) Del(c *gin.Context) {
 //	@Summary	监控-错误列删除-批量
 //	@Tags		monitor_slow-监控-错误列
 //
-// @Produce	json
-// @Param		Token		header		string				true	"token"
-// @Param		Ids		body		string				false	"逗号分割的id"
-// @Success	200			{object}	response.Response	"成功"
-// @Router		/api/admin/monitor_slow/delBatch [post]
+//	@Produce	json
+//	@Param		Token	header		string				true	"token"
+//	@Param		Ids		body		string				false	"逗号分割的id"
+//	@Success	200		{object}	response.Response	"成功"
+//	@Router		/api/admin/monitor_slow/delBatch [post]
 func (hd *MonitorSlowHandler) DelBatch(c *gin.Context) {
 	var delReq MonitorSlowDelBatchReq
 	if response.IsFailWithResp(c, util.VerifyUtil.VerifyJSON(c, &delReq)) {
@@ -173,18 +167,18 @@ func (hd *MonitorSlowHandler) DelBatch(c *gin.Context) {
 	response.CheckAndResp(c, MonitorSlowService.DelBatch(Ids))
 }
 
-// @Summary	监控-错误列导出
-// @Tags		monitor_slow-监控-错误列
-// @Produce	json
-// @Param		Token		header		string				true	"token"
-// @Param ProjectKey query string false "项目key"
-// @Param ClientId query string false "sdk生成的客户端id"
-// @Param UserId query string false "用户id"
-// @Param Path query string false "URL地址"
-// @Param Time query number false "时间"
-// @Param CreateTimeStart  query string false "创建时间"
-// @Param CreateTimeEnd  query string false "创建时间"
-// @Router		/api/admin/monitor_slow/ExportFile [get]
+//	@Summary	监控-错误列导出
+//	@Tags		monitor_slow-监控-错误列
+//	@Produce	json
+//	@Param		Token			header	string	true	"token"
+//	@Param		ProjectKey		query	string	false	"项目key"
+//	@Param		ClientId		query	string	false	"sdk生成的客户端id"
+//	@Param		UserId			query	string	false	"用户id"
+//	@Param		Path			query	string	false	"URL地址"
+//	@Param		Time			query	number	false	"时间"
+//	@Param		CreateTimeStart	query	string	false	"创建时间"
+//	@Param		CreateTimeEnd	query	string	false	"创建时间"
+//	@Router		/api/admin/monitor_slow/ExportFile [get]
 func (hd *MonitorSlowHandler) ExportFile(c *gin.Context) {
 	var listReq MonitorSlowListReq
 	if response.IsFailWithResp(c, util.VerifyUtil.VerifyQuery(c, &listReq)) {
@@ -203,10 +197,10 @@ func (hd *MonitorSlowHandler) ExportFile(c *gin.Context) {
 	excel2.DownLoadExcel("监控-错误列"+time.Now().Format("20060102-150405"), c.Writer, f)
 }
 
-//	 @Summary	监控-错误列导入
-//	 @Tags		monitor_slow-监控-错误列
-//	 @Produce	json
-//		@Router		/api/admin/monitor_slow/ImportFile [post]
+//	@Summary	监控-错误列导入
+//	@Tags		monitor_slow-监控-错误列
+//	@Produce	json
+//	@Router		/api/admin/monitor_slow/ImportFile [post]
 func (hd *MonitorSlowHandler) ImportFile(c *gin.Context) {
 	file, _, err := c.Request.FormFile("file")
 	if err != nil {
